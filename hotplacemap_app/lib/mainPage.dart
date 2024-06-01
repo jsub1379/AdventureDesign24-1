b72d913498083b21c6749030f554d425760feabf
Sangwon Jung <codehotel@DeskHotel.DeskHotel.net>
2024-06-02 06:16:01 +0900

    Aesthetic changes
    Issue #2

--- hotplacemap_app/lib/mainPage.dart
+++ hotplacemap_app/lib/mainPage.dart
@@ -31,7 +31,13 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-
+    final dotSize = screenHeight * 0.003;
+    final dotMargin = screenHeight * 0.001;
+    final innerMargin = EdgeInsets.fromLTRB(
+        screenWidth * 0.005,
+        screenHeight * 0.00,
+        screenWidth * 0.005,
+        screenHeight * 0.001);
     return Scaffold(
       body: PageView(
         controller: _pageController,
@@ -43,58 +49,75 @@ class _MainPageState extends State<MainPage> {
         children: _pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: [
           BottomNavigationBarItem(
-            icon: Container(
-              padding: EdgeInsets.fromLTRB(
-                  screenWidth * 0.005,
-                  screenHeight * 0.001,
-                  screenWidth * 0.005,
-                  screenHeight * 0.001
-              ),
-              margin: EdgeInsets.zero,
-              child: Image.asset(
-                'assets/navbar_map_deselected.png',
-                width: screenWidth * 0.06,
-                height: screenHeight * 0.04,
-                color: _selectedIndex == 0 ? const Color(0xFFEF6C00) : null,
-              ),
+            icon: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/navbar_map_deselected.png',
+                  width: screenWidth * 0.06,
+                  height: screenHeight * 0.04,
+                  color: _selectedIndex == 0 ? const Color(0xFFEF6C00) : null,
+                ),
+                SizedBox(height: dotMargin),
+                Container(
+                  width: screenWidth * 0.03,
+                  height: screenHeight * 0.004,
+                  decoration: BoxDecoration(
+                    color: _selectedIndex == 0 ? const Color(0xFFEF6C00) : Colors.transparent,
+                    borderRadius: BorderRadius.circular(screenWidth * 0.02),
+                  ),
+                ),
+              ],
             ),
             label: '검색',
           ),
           BottomNavigationBarItem(
-            icon: Container(
-              padding: EdgeInsets.fromLTRB(
-                  screenWidth * 0.005,
-                  screenHeight * 0.001,
-                  screenWidth * 0.005,
-                  screenHeight * 0.001
-              ),
-              margin: EdgeInsets.zero,
-              child: Image.asset(
-                'assets/navbar_fav_deselected.png',
-                width: screenWidth * 0.06,
-                height: screenHeight * 0.04,
-                color: _selectedIndex == 1 ? const Color(0xFFEF6C00) : null,
-              ),
+            icon: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/navbar_fav_deselected.png',
+                  width: screenWidth * 0.06,
+                  height: screenHeight * 0.04,
+                  color: _selectedIndex == 1 ? const Color(0xFFEF6C00) : null,
+                ),
+                SizedBox(height: dotMargin),
+                Container(
+                  width: screenWidth * 0.03,
+                  height: screenHeight * 0.004,
+                  decoration: BoxDecoration(
+                    color: _selectedIndex == 1 ? const Color(0xFFEF6C00) : Colors.transparent,
+                    borderRadius: BorderRadius.circular(screenWidth * 0.02),
+                  ),
+                ),
+              ],
             ),
             label: '즐겨찾기',
           ),
           BottomNavigationBarItem(
-            icon: Container(
-              padding: EdgeInsets.fromLTRB(
-                  screenWidth * 0.005,
-                  screenHeight * 0.001,
-                  screenWidth * 0.005,
-                  screenHeight * 0.001
-              ),
-              margin: EdgeInsets.zero,
-              child: Image.asset(
-                'assets/navbar_account_deselected.png',
-                width: screenWidth * 0.06,
-                height: screenHeight * 0.04,
-                color: _selectedIndex == 2 ? const Color(0xFFEF6C00) : null,
-              ),
+            icon: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/navbar_account_deselected.png',
+                  width: screenWidth * 0.06,
+                  height: screenHeight * 0.04,
+                  color: _selectedIndex == 2 ? const Color(0xFFEF6C00) : null,
+                ),
+                SizedBox(height: dotMargin),
+                Container(
+                  width: screenWidth * 0.03,
+                  height: screenHeight * 0.004,
+                  decoration: BoxDecoration(
+                    color: _selectedIndex == 2 ? const Color(0xFFEF6C00) : Colors.transparent,
+                    borderRadius: BorderRadius.circular(screenWidth * 0.02),
+                  ),
+                ),
+              ],
             ),
             label: '개인정보',
           ),
@@ -103,6 +126,8 @@ class _MainPageState extends State<MainPage> {
         onTap: _onItemTapped,
         selectedItemColor: Colors.deepOrange,
       ),
+
+
     );
   }
 }
